478fd7b40cb121758385cdd9dd7cc1977c73e957
Bob Verschoor <bob.verschoor@alliander.com>
2023-12-01 15:08:35 +0100

    Opschoning en refactoring

--- dag1/dag1.py
+++ dag1/dag1.py
@@ -1,10 +1,12 @@
-LITERAL = ['one','two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-LITERAL_REV = ['eno','owt','eerht', 'ruof', 'evif', 'xis', 'neves', 'thgie', 'enin']
+LITERAL = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+LITERAL_REV = ['eno', 'owt', 'eerht', 'ruof', 'evif', 'xis', 'neves', 'thgie', 'enin']
+
 
 def get_first_digit(line):
     for c in line:
         if c.isdigit():
             return c
+
 
 def get_last_digit(line):
     for c in reversed(line):
@@ -22,6 +24,7 @@ def get_last_literal(line):
         if c.isdigit():
             return c
 
+
 def get_first_literal(line):
     parsed = ""
     for c in line:
@@ -32,24 +35,32 @@ def get_first_literal(line):
         if c.isdigit():
             return c
 
+
+def get_calibration_value(line):
+    first_digit = get_first_digit(line)
+    last_digit = get_last_digit(line)
+    return int(first_digit + last_digit)
+
+
+def get_calibration_value_literal(line):
+    first_digit = get_first_literal(line)
+    last_digit = get_last_literal(line)
+    return int(first_digit + last_digit)
+
+
 class CalibrationDocument:
-    def __int__(self):
-        self._lines = []
+    def __init__(self):
+        self._lines = None
 
     def readinput(self):
-        input = open('input.txt', 'r')
-        self._lines = input.readlines()
+        inputfile = open('input.txt', 'r')
+        self._lines = inputfile.readlines()
 
     def get_calibration_values(self):
         cals = []
         for line in self._lines:
-            cals.append(self.get_calibration_value(line))
+            cals.append(get_calibration_value(line))
         return cals
-
-    def get_calibration_value(self, line):
-        first_digit = get_first_digit(line)
-        last_digit = get_last_digit(line)
-        return int(first_digit + last_digit)
 
     def sum_calibration_values(self):
         total = 0
@@ -57,15 +68,10 @@ class CalibrationDocument:
             total += cv
         return total
 
-    def get_calibration_value_literal(self, line):
-        first_digit = get_first_literal(line)
-        last_digit = get_last_literal(line)
-        return int(first_digit + last_digit)
-
     def get_calibration_values_literal(self):
         cals = []
         for line in self._lines:
-            cals.append(self.get_calibration_value_literal(line))
+            cals.append(get_calibration_value_literal(line))
         return cals
 
     def sum_calibration_values_literal(self):
